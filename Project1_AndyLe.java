54e66209148e579481a8d23693eab4fed90502ac
Andy Le <bndy123@gmail.com>
2020-01-23 20:32:44 -0600

    my first commit u fucker!

---
+++ Project1_AndyLe.java
@@ -0,0 +1,22 @@
+
+public class Project1_AndyLe {
+	/**   * This is my first Java program.  
+	 * * @author Andy Le 
+	 * * @version 1.0  
+	 * */ 
+	
+	// This is the main method. The instructions inside  
+	// the braces are executed when the program runs. 
+	
+	public static void main (String[] args)
+	{
+		int iSmall = 3;
+		int iLarge = 7;
+		double dSmall = 2.0;
+		double dLarge = 8.5;
+		
+		int number = iLarge + iSmall / dSmall – dLarge;
+		System.out.print(number);
+	}
+	
+}
